4179ab756304c2f57b2d6ee377c943feced2d8b7
myka1 <msmykolas@gmail.com>
2023-06-14 16:09:03 +0300

    react-redux
    learning redux

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Header from "./components/Header";
+import Counter from "./components/Counter";
+import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
+
+import { useSelector } from "react-redux";
+
+function App() {
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
+  return (
+    <>
+      <Header />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
+      <Counter />
+    </>
+  );
+}
+
+export default App;
